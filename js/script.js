7ccab61bdfd718f7959ab4f3e26b435963601143
Bektur13 <duyshembekov04@mail.ru>
2021-03-13 17:34:57 +0600

    added files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+TweenMax.from(".header_top",1.5,{
+    opacity: 0,
+    x:-40,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".top__line",2,{
+    opacity: 0,
+    y: -40,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".header__logo",1.8,{
+    opacity: 0,
+    x: 100,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".header__search",2,{
+    opacity: 0,
+    x: 100,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".header__svg",2.2,{
+    opacity: 0,
+    x: 100,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".bottom__ul li",2.2,{
+    opacity: 0,
+    x: -100,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".hero",2.2,{
+    opacity: 0,
+    y: 100,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".grid",2.8,{
+    opacity: 0,
+    x: 500,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".list_view",2.8,{
+    opacity: 0,
+    x: 500,
+    ease:Expo.easeInOut
+})
+TweenMax.from(".bill",2.8,{
+    opacity: 0,
+    x: 500,
+    ease:Expo.easeInOut
+})
